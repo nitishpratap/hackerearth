208e1fd770aa9ec4a1b529e6cb6ba365a1ffc157
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-02-23 11:32:09 +0530

    Add files via upload

---
+++ movement/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // cout << "Hello world!" << endl;
+       int n;
+    cin>>n;
+
+    int num=n/5+1;
+    cout<<num<<endl;
+    return 0;
+}
